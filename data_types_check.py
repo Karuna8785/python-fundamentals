9d6bfd301ed2acdc0f35897fa8c505fd4cefb3cf
Karuna8785 <karunagujjalapudi8@gmail.com>
2026-07-01 22:13:13 +0530

    Add data type checks for various variables

---
+++ data_types_check.py
@@ -0,0 +1,17 @@
+a = 25
+b = 3.14
+c = "Python"
+d = True
+e = [1, 2, 3]
+f = (10, 20)
+g = {"name": "Karuna"}
+h = {1, 2, 3}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
